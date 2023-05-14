a6cf7740bade567d1d445788d1995dfbba7e55f0
Eugenio <chisari@cs.uni-freiburg.de>
2023-05-14 16:48:02 +0200

    inference_time

--- contact_graspnet/run_inference.py
+++ contact_graspnet/run_inference.py
@@ -1,3 +1,4 @@
+import time
 import pathlib
 import numpy as np
 from PIL import Image
@@ -24,15 +25,18 @@ def main():
     depth = np.array(Image.open(data_path / "depth.png"))
     depth = depth.astype(np.float32) / 1000.0
     depth_K = ZED2_INTRINSICS_HALF
-    segmap = None
     contactgraspnet = ContactGraspNetInference()
+    start_time = time.time()
+    segmap = None
     pc_full, pc_colors, pred_grasps_cam, scores = contactgraspnet.predict(
         rgb_uint8, depth, depth_K, segmap
     )
+    inference_time = time.time() - start_time
     np.save(data_path / "pc_full.npy", pc_full)
     np.save(data_path / "pc_colors.npy", pc_colors)
     np.save(data_path / "pred_grasp_cam.npy", pred_grasps_cam[-1])
     np.save(data_path / "scores.npy", scores[-1])
+    np.save(data_path / "inference_time.npy", inference_time)
     # contactgraspnet.visualize_results(
     #     rgb_uint8, segmap, pc_full, pc_colors, pred_grasps_cam, scores
     # )
